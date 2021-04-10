5951fc1ea1d69a855ae3710dfc5263cd947565f7
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-04-10 21:20:44 +0300

    Add extension methods for Telegram Bot Engine

---
+++ DependencyInjection/TelegramBotEngineExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Helper.Options;
+
+namespace Telegram.Bot.Helper.DependencyInjection
+{
+    public static class TelegramBotEngineExtensions
+    {
+        public static IServiceCollection AddTelegramBotEngine(this IServiceCollection services,
+            Action<TelegramBotOptions> configure)
+        {
+            services.Configure(configure);
+            services.AddSingleton<ITelegramBotCommandManager, AttributeFilterCommandManager>();
+            services.AddSingleton<IUpdateHandler, DependencyInjectionUpdateHandler>();
+            return services;
+        }
+        
+        public static IServiceCollection AddTelegramBotEngine(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<TelegramBotOptions>(configuration.GetSection("TelegramBot"));
+            services.AddSingleton<ITelegramBotCommandManager, AttributeFilterCommandManager>();
+            services.AddSingleton<IUpdateHandler, DependencyInjectionUpdateHandler>();
+            return services;
+        }
+    }
+}
