e4837e691f9e4eb37a82ab348b3033d991730f11
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-04-11 17:48:05 +0300

    Introduce message base filter

--- src/Telegram.Bot.Helper/Attributes/TelegramBotFilterAttribute.cs
+++ src/Telegram.Bot.Helper/Attributes/TelegramBotFilterAttribute.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
 
 namespace Telegram.Bot.Helper.Attributes
 {
     public abstract class TelegramBotFilterAttribute : Attribute
     {
+        public Update Update { get; internal set; }
+        
+        public virtual TelegramBotFilterAttribute[] ParentFilters { get; }
+        
         protected internal virtual bool IsValid() => true;
 
         protected internal virtual ValueTask<bool> IsValidAsync() => new ValueTask<bool>(true);
---
+++ src/Telegram.Bot.Helper/Attributes/TelegramBotMessageFilterAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot.Types;
+
+namespace Telegram.Bot.Helper.Attributes
+{
+    public abstract class TelegramBotMessageFilterAttribute : TelegramBotFilterAttribute
+    {
+        public Message Message => Update.Message;
+
+        protected internal override bool IsValid() => Message != null;
+    }
+}
